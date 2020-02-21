52a56cbed9a01faa9cde54813e03ed7d225388f1
Johan Back <johan.backs@gmail.com>
2020-02-21 17:55:15 -0300

    chore: add numjucks

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
 const express = require("express");
 const server = express();
 
+server.use(express.static("public"));
+
+//template engine - nunjucks
+const nunjuncks = require("nunjucks")
+nunjuncks.configure("./",{
+  express:server
+});
+
+// LISTA DE DOADORES
+const donors = [
+  {
+    name: "Johan Back",
+    blood: "AB+"
+  },
+  {
+    name: "Usuario Sobrenome",
+    blood: "O+"
+  },
+  {
+    name: "Usuario Sobrenome",
+    blood: "A-"
+  },
+  {
+    name: "Usuario Sobrenome",
+    blood: "B-"
+  },
+]
+
+
+
 server.get("/", function(req, res){
-  return res.send("ok dev3")
+  return res.render("index.html", {donors})
 });
 
 //Ligar servidor porta 3000
-server.listen(3000);
+server.listen(3000, function(){
+  console.log("Ok")
+});
 
