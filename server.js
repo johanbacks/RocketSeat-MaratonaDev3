a1bc24c7507ad6e57ebb2e4ff81e591ed052de2b
Johan Back <johan.backs@gmail.com>
2020-02-24 15:08:50 -0300

    chore: add end MaratonaDev3

--- server.js
+++ server.js
@@ -1,38 +1,61 @@
 const express = require("express");
 const server = express();
-
+//arquivos staticos da pasta public
 server.use(express.static("public"));
+
+//express habilitar body
+server.use(express.urlencoded({ extended:true }));
+
+// Conf conection DB
+const Pool =  require("pg").Pool;
+const db = new Pool({
+  user: "postgres",
+  password: "0808",
+  host:"localhost",
+  port: 5432,
+  database: "doe"
+})
 
 //template engine - nunjucks
 const nunjuncks = require("nunjucks")
 nunjuncks.configure("./",{
-  express:server
+  express:server,
+  noCache:true,
 });
 
-// LISTA DE DOADORES
-const donors = [
-  {
-    name: "Johan Back",
-    blood: "AB+"
-  },
-  {
-    name: "Usuario Sobrenome",
-    blood: "O+"
-  },
-  {
-    name: "Usuario Sobrenome",
-    blood: "A-"
-  },
-  {
-    name: "Usuario Sobrenome",
-    blood: "B-"
-  },
-]
 
 
 
 server.get("/", function(req, res){
-  return res.render("index.html", {donors})
+  
+  db.query("SELECT * FROM donors", function(err,result){
+    if (err) return res.send("Erro de banco de dados.");
+    const donors = result.rows; 
+    return res.render("index.html", {donors})
+  })
+});
+
+server.post("/", function(req, res){
+  const name = req.body.name 
+  const email = req.body.email 
+  const blood = req.body.blood  
+
+  if (name == "" || email == "" || blood == ""){  
+    return res.send("Todos os campos são obrigadorios")
+  }
+  
+  const query = `INSERT INTO donors ("name", "email", "blood") 
+                  VALUES($1,$2,$3 )`
+
+  const values = [name,email,blood];
+  db.query(query ,values , function(err){
+    if (err) 
+    return res.send("Erro no banco de dados.")
+
+    return res.redirect("/")
+   
+  });
+ 
 });
 
 //Ligar servidor porta 3000
